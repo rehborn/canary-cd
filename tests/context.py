d524e48a0eea885e65a60b86180b0b4963cd2a5b
Dominique Rehborn <dominique@rehborn.dev>
2025-08-24 14:15:05 +0200

    refactor and cleanup context, overwrite HTTPD_CONFIG_DUMP and DATA_DIR

--- tests/context.py
+++ tests/context.py
@@ -3,33 +3,25 @@ import tempfile
 
 import pytest
 import sys
-from fastapi.testclient import TestClient
 from httpx import ASGITransport, AsyncClient
-# from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
 import logging
 
+ROOT_KEY = 'test'
+os.environ['ROOT_KEY'] = ROOT_KEY
+os.environ['HTTPD_CONFIG_DUMP'] = '1'
+
+temp = tempfile.TemporaryDirectory(delete=False)
+os.environ['DATA_DIR'] = temp.name
+
+from canary_cd import settings
 from canary_cd.main import lifespan
 
 logger = logging.getLogger(__name__)
-
-# Overwrite auto generation for SALT and API_KEY settings
-SALT = 'DWlKl7lBstE5M/FEv0v6Fx53nZDF0jJkLMURflbIRSw='
-ROOT_KEY = 'test'
-
-os.environ.setdefault('SALT', SALT)
-os.environ.setdefault('ROOT_KEY', ROOT_KEY)
-
-temp = tempfile.TemporaryDirectory(delete=False)
-logger.debug(f'DATA_DIR: {temp.name}')
-
-os.environ.setdefault('DATA_DIR', temp.name)
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from canary_cd.main import app  # pylint: disable=import-error,wrong-import-position
 from canary_cd.database import *
-from canary_cd.utils.crypto import CryptoHelper
 
 
 @pytest.fixture(scope="session")
@@ -43,9 +35,7 @@ async def client_fixture(session: Session):
         return session
 
     app.dependency_overrides[get_session] = get_session_override  # noqa
-
     headers = {"Authorization": f"Bearer {ROOT_KEY}"}
-
     transport = ASGITransport(app=app)
     async with lifespan(app):
         async with AsyncClient(transport=transport, headers=headers, base_url="http://test") as client:
@@ -55,21 +45,11 @@ async def client_fixture(session: Session):
 
 @pytest.fixture(name="session", scope="session")
 async def session_fixture():
+    sqlite = 'sqlite:///{}/database.sqlite'.format(settings.DATA_DIR.absolute())
     engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+        sqlite,
+        connect_args={"check_same_thread": False},
     )
     SQLModel.metadata.create_all(engine)
-    db = Session(engine)
-
-    # set initial ROOT_KEY
-    root_key = Config(key='ROOT_KEY', value=CryptoHelper(SALT).hash(ROOT_KEY))
-    db.add(root_key)
-    db.commit()
-    return db
-    # create_db_and_tables()
-    # with Session(engine) as session:
-    #     yield session
-#
-#
-# headers = {"Authorization": f"Bearer {ROOT_KEY}"}
-# client = TestClient(app, headers=headers)
+    with Session(engine) as session:
+        yield session
