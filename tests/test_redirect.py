1675e35b50a6c29c2595ef11e11968d541d56549
Dominique Rehborn <dominique@rehborn.dev>
2025-08-24 14:31:46 +0200

    refactor redirect tests, check for already existing, check update on existing and invalid redirect

--- tests/test_redirect.py
+++ tests/test_redirect.py
@@ -6,6 +6,7 @@ from canary_cd.models import RedirectDetails
 TEST_SOURCE = 'example.com'
 TEST_DEST = 'www.example.com'
 
+
 class TestRedirectAPI:
     @pytest.mark.anyio
     @pytest.mark.dependency()
@@ -14,14 +15,37 @@ class TestRedirectAPI:
         assert response.status_code == 201
         data = response.json()
         assert data['source'] == TEST_SOURCE
-        # for field in RedirectDetails.model_fields.keys():
-        #     assert field in data
 
     @pytest.mark.anyio
     @pytest.mark.dependency(depends=['TestRedirectAPI::test_redirect_create'])
     async def test_redirect_create_already_exists(self, client: AsyncClient, session: Session):
         response = await client.post("/redirect", json={'source': TEST_SOURCE, 'destination': TEST_DEST})
         assert response.status_code == 403
+        assert response.json()['detail'] == 'Redirect already exists'
+
+    @pytest.fixture()
+    def create_page_dummy(self, session: Session):
+        page = Page(**{'fqdn': 'existing-page.com'})
+        session.add(page)
+        session.commit()
+
+    @pytest.mark.anyio
+    @pytest.mark.usefixtures('create_page_dummy')
+    async def test_redirect_create_existing_page(self, client: AsyncClient):
+        response = await client.post("/redirect", json={'source': 'existing-page.com', 'destination': TEST_DEST})
+        assert response.status_code == 403
+        assert response.json()['detail'] == 'Page with this FQDN already exists'
+
+    @pytest.mark.anyio
+    @pytest.mark.dependency(depends=['TestRedirectAPI::test_redirect_create'])
+    async def test_page_update(self, client: AsyncClient):
+        response = await client.put(f"/redirect/{TEST_SOURCE}", json={'destination': TEST_DEST})
+        assert response.status_code == 200
+
+    @pytest.mark.anyio
+    async def test_page_update_invalid(self, client: AsyncClient):
+        response = await client.put(f"/redirect/does-not-exist.com", json={'destination': TEST_DEST})
+        assert response.status_code == 404
 
     @pytest.mark.anyio
     @pytest.mark.dependency(depends=['TestRedirectAPI::test_redirect_create'])
@@ -29,10 +53,7 @@ class TestRedirectAPI:
         response = await client.get('/redirect')
         data = response.json()
         assert response.status_code == 200
-        assert len(data) == 1
         assert type(data) == list
-        # for field in RedirectDetails.model_fields.keys():
-        #     assert field in data[0]
 
     @pytest.mark.anyio
     @pytest.mark.dependency(depends=['TestRedirectAPI::test_redirect_create'])
