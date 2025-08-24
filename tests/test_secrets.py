82c0c9855636052e3e6a7557e14b5ec494d8a347
Dominique Rehborn <dominique@rehborn.dev>
2025-08-24 14:46:05 +0200

    refactor secret tests, create project dummy, check updating listing and deleting invalid project and key

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -8,26 +8,31 @@ TEST_PROJECT = {'name': TEST_NAME, 'remote': TEST_REMOTE, 'branch': 'main'}
 TEST_SECRET = {'key': 'KEY', 'value': 'VALUE'}
 
 class TestSecretAPI:
-    @pytest.mark.anyio
-    @pytest.mark.dependency()
-    async def test_project_create_for_secrets(self, client: AsyncClient, session: Session):
-        response = await client.post("/project", json=TEST_PROJECT)
-        data = response.json()
-        assert 'name' in data.keys()
-        assert 'remote' in data.keys()
-        assert response.status_code == 201
+    @pytest.fixture()
+    def create_project_dummy(self, session: Session):
+        project = Project(**TEST_PROJECT)
+        session.add(project)
+        session.commit()
 
     @pytest.mark.anyio
-    @pytest.mark.dependency(depends=['TestSecretAPI::test_project_create_for_secrets'])
-    async def test_secret_create(self, client: AsyncClient, session: Session):
+    @pytest.mark.dependency()
+    @pytest.mark.usefixtures('create_project_dummy')
+    async def test_secret_set(self, client: AsyncClient, session: Session):
         response = await client.put(f"/secret/{TEST_NAME}", json=TEST_SECRET)
         data = response.json()
         assert 'key' in data.keys()
         assert response.status_code == 200
 
     @pytest.mark.anyio
-    @pytest.mark.dependency(depends=['TestSecretAPI::test_secret_create'])
-    async def test_secret_view(self, client: AsyncClient, session: Session):
+    async def test_secret_set_invalid_project(self, client: AsyncClient, session: Session):
+        response = await client.put(f"/secret/does-not-exist", json=TEST_SECRET)
+        data = response.json()
+        assert response.status_code == 403
+        assert data['detail'] == 'Project does not exists'
+
+    @pytest.mark.anyio
+    @pytest.mark.dependency(depends=['TestSecretAPI::test_secret_set'])
+    async def test_secret_list(self, client: AsyncClient, session: Session):
         response = await client.get(f"/secret/{TEST_NAME}")
         data = response.json()
         assert response.status_code == 200
@@ -39,9 +44,31 @@ class TestSecretAPI:
         assert data[0]['value'] == TEST_SECRET['value']
 
     @pytest.mark.anyio
-    @pytest.mark.dependency(depends=['TestSecretAPI::test_secret_create'])
+    async def test_secret_list_invalid_project(self, client: AsyncClient, session: Session):
+        response = await client.get(f"/secret/does-not-exist")
+        data = response.json()
+        assert response.status_code == 403
+        assert data['detail'] == 'Project does not exists'
+
+    @pytest.mark.anyio
+    @pytest.mark.dependency(depends=['TestSecretAPI::test_secret_set'])
     async def test_secret_delete(self, client: AsyncClient):
         response = await client.delete(f'/secret/{TEST_NAME}/{TEST_SECRET["key"]}')
         data = response.json()
         assert response.status_code == 200
         assert data['detail'] == f'{TEST_SECRET["key"]} deleted'
+
+    @pytest.mark.anyio
+    @pytest.mark.dependency(depends=['TestSecretAPI::test_secret_set'])
+    async def test_secret_delete_invalid_key(self, client: AsyncClient):
+        response = await client.delete(f'/secret/{TEST_NAME}/invalid-key')
+        data = response.json()
+        assert response.status_code == 403
+        assert data['detail'] == 'Secret does not exists'
+
+    @pytest.mark.anyio
+    async def test_secret_delete_invalid_project(self, client: AsyncClient):
+        response = await client.delete(f'/secret/does-not-exist/key')
+        data = response.json()
+        assert response.status_code == 403
+        assert data['detail'] == 'Project does not exists'
